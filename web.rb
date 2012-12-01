bb402e09b3718b045671164a3ab579e37bf3030f
Evan Weaver <evan@cloudbur.st>
2012-11-30 17:31:32 -0800

    TAke a param

--- web.rb
+++ web.rb
@@ -4,7 +4,7 @@ require "atom/pub"
 require "open-uri"
 require "sinatra/reloader"
 
-get '/giants/:whatever' do
+get '/giants/:keyword.atom' do
   feed = Atom::Feed.new do |f|
     f.title = "SF Giants Seat Marketplace"
     f.id = "http://sfgiants.seasonticketrights.com/Charter-Seat-Licenses/Charter-Seat-Licenses.aspx"
@@ -13,10 +13,10 @@ get '/giants/:whatever' do
     open("http://sfgiants.seasonticketrights.com/Charter-Seat-Licenses/Charter-Seat-Licenses.aspx") do |g|
       g.read.scan(/title=\".*?\"/) do |m|
         if m =~ /Listing/
-          next unless m =~ /Club/
+          next unless m =~ /#{params["keyword"]}/i
           seats = m[/Seat Number\(s\)(.*)"/, 1].split(" - ")
-          puts seats.inspect
-          next unless seats.first.to_i - seats.last.to_i == -1
+#          puts seats.inspect
+#          next unless seats.first.to_i - seats.last.to_i == -1
           f.entries << Atom::Entry.new do |e|
             m = m[7..-2]
             e.id = m[/(\d+)/, 1]
